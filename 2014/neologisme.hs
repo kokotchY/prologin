1f41410cb38647a5b92f275a675b3c3b26cb36f4
Sebastien Canart <sebastien.canart@onprvp.fgov.be>
2016-01-09 20:57:03 +0100

    Update script to use less memory

--- 2014/neologisme.hs
+++ 2014/neologisme.hs
@@ -1,17 +1,15 @@
+{-# LANGUAGE BangPatterns #-}
 module Main where
 
 import Control.Monad
 
 main :: IO ()
 main = do
-    _ <- getLine
+    getLine
     word <- getLine
     str_n <- getLine
     let n = read str_n
-    dictionary <- replicateM n $ do
-        _ <- getLine
-        dico_word <- getLine
-        return dico_word
+    !dictionary <- replicateM n $ getLine >> getLine
     if word `elem` dictionary
         then print 0
         else print 1

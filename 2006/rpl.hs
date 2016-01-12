39e2e5c8397c8131919f5675fcedb4ff6e78cf8c
Sebastien Canart <sebastien.canart@onprvp.fgov.be>
2016-01-12 18:50:21 +0100

    More implementation of rpl

--- 2006/rpl.hs
+++ 2006/rpl.hs
@@ -3,31 +3,73 @@ module Main where
 import Control.Monad
 import Data.Char
 
-type Programme = [String]
-type Stack = [String]
+type Programme = [Operation]
+type Stack = [Number]
 
-execute_op :: String -> Stack -> Stack
-execute_op op (x:y:xs)
-    | op == "+" = show ((read x) + (read y)) : xs
-    | op == "-" = show ((read x) - (read y)) : xs
+data Number = Decimal Int
+    | Binary [Int]
+    | Hexa [Char]
+    | Octal [Int]
+    deriving Show
 
-is_operator ::  String -> Bool
-is_operator "+" = True
-is_operator _ = False
+data Operation = Drop
+    | Dup
+    | Swap
+    | Add
+    | Min
+    | Mul
+    | Div
+    deriving Show
 
-is_digit :: String -> Bool
-is_digit = all is_nb
+data BoolOperation = BValue Bool
+    | Less
+    | LessEq
+    | Greater
+    | GreaterEq
+    | Equal
+    | Different
+    deriving Show
 
-is_nb :: Char -> Bool
-is_nb nb = ord nb >= ord '0' && ord nb <= ord '9'
 
-parse :: Programme -> Stack -> [String]
-parse [] stack = stack
-parse (x:xs) stack
-    | is_digit x = parse xs (x:stack)
-    | is_operator x = parse xs (execute_op x stack)
+
+nbPlus :: Number -> Number -> Number
+nbPlus (Decimal a) (Decimal b) = Decimal $ a + b
+nbPlus _ _ = error "Not implemented"
+
+nbMin :: Number -> Number -> Number
+nbMin (Decimal a) (Decimal b) = Decimal $ a - b
+nbMin _ _ = error "Not implemented"
+
+nbMul :: Number -> Number -> Number
+nbMul (Decimal a) (Decimal b) = Decimal $ a * b
+nbMul _ _ = error "Not implemented"
+
+nbDiv :: Number -> Number -> Number
+nbDiv (Decimal a) (Decimal b) = Decimal $ a `div` b
+nbDiv _ _ = error "Not implemented"
+
+execOp :: (Number -> Number -> Number) -> Stack -> Stack
+execOp f (nb1:nb2:stack) = new_val:stack
+    where
+        new_val = f nb1 nb2
+
+swap :: Stack -> Stack
+swap (x:y:xs) = (y:x:xs)
+
+eval :: Programme -> Stack -> Stack
+eval [] stack = stack
+eval (x:xs) stack =
+    case x of
+        Add -> eval xs $ execOp nbPlus stack
+        Min -> eval xs $ execOp nbMin stack
+        Mul -> eval xs $ execOp nbMul stack
+        Div -> eval xs $ execOp nbDiv stack
+        Drop -> eval xs $ tail stack
+        Dup -> eval xs (head stack:stack)
+        Swap -> eval xs $ swap stack
 
 prog1 = "1 2 +"
+
 
 main :: IO ()
 main = do

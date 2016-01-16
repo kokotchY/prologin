ce2daabee4f914c7570c34502cbecb063d75b33d
Sebastien Canart <sebastien.canart@onprvp.fgov.be>
2016-01-16 15:27:15 +0100

    Add variables

--- 2006/rpl.hs
+++ 2006/rpl.hs
@@ -3,9 +3,18 @@ module Main where
 import Control.Monad
 import Data.Char
 import Data.List
+import qualified Data.Map.Strict as Map
 
 type Programme = [Instruction]
 type Stack = [Instruction]
+type Variables = Map.Map VariableName Instruction
+type VariableName = String
+
+data State = State
+    { getStack :: Stack
+    , getVariable :: Variables
+    }
+    deriving Show
 
 data Number = Decimal Int
     | Binary [Int]
@@ -36,15 +45,31 @@ data Instruction = Nb Number
     | StackOp SOperation
     | NumberOp NOperation
     | BoolOp BOperation
+    | AssignVariable VariableName
+    | GetVariable VariableName
     deriving (Show, Eq)
 
-execOpNumber :: (Int -> Int -> Int) -> Stack -> Stack
-execOpNumber f (Nb (Decimal a):Nb (Decimal b):xs) = Nb (Decimal (f a b)):xs
+execOpNumber :: (Int -> Int -> Int) -> State -> State
+execOpNumber f state = state { getStack = Nb (Decimal (f a b)):remainStack }
+    where
+        stack = getStack state
+        a = case (head stack) of
+            Nb (Decimal x) -> x
+        b = case (head $ tail stack) of
+            Nb (Decimal x) -> x
+        remainStack = drop 2 stack
 
-execOpBool :: (Int -> Int -> Bool) -> Stack -> Stack
-execOpBool f (Nb (Decimal a):Nb (Decimal b):xs)
-    | f a b = Nb (Decimal 1):xs
-    | otherwise = Nb (Decimal 0):xs
+execOpBool :: (Int -> Int -> Bool) -> State -> State
+execOpBool f state
+    | f a b = state { getStack = Nb (Decimal 1):remainStack }
+    | otherwise = state { getStack = Nb (Decimal 0):remainStack }
+    where
+        stack = getStack state
+        a = case (head stack) of
+            Nb (Decimal x) -> x
+        b = case (head $ tail stack) of
+            Nb (Decimal x) -> x
+        remainStack = drop 2 stack
 
 swap :: Stack -> Stack
 swap (x:y:xs) = (y:x:xs)
@@ -54,53 +79,76 @@ convertDecimal (Decimal x) = Decimal x
 convertDecimal (Binary x) = Decimal $ foldl' (\x y -> x*2+y) 0 x
 convertDecimal _ = error "Not implemented"
 
-appendStack :: Stack -> Instruction -> Stack
-appendStack stack op = op:stack
+appendStack :: State -> Instruction -> State
+appendStack state op = state { getStack = op:(getStack state) }
 
-eval :: Programme -> Stack -> Stack
-eval [] stack = stack
-eval (x:xs) stack =
+addVariable :: String -> State -> State
+addVariable name state = state { getVariable = variables, getStack = newStack }
+    where
+        variables = Map.insert name (head stack) (getVariable state)
+        stack = getStack state
+        newStack = tail stack
+
+appendVariableStack :: String -> State -> State
+appendVariableStack name state = state { getStack = newStack }
+    where
+        stack = getStack state
+        variableValue = case Map.lookup name (getVariable state) of
+            Just value -> value
+        newStack = variableValue:stack
+
+eval :: Programme -> State -> State
+eval [] state = state
+eval (x:xs) state =
     case x of
-        Nb number -> eval xs $ appendStack stack (Nb $ convertDecimal number)
+        Nb number -> eval xs $ appendStack state (Nb $ convertDecimal number)
         NumberOp op ->
             case op of
-                Add -> eval xs $ execOpNumber (+) stack
-                Min -> eval xs $ execOpNumber (-) stack
-                Mul -> eval xs $ execOpNumber (*) stack
-                Div -> eval xs $ execOpNumber (div) stack
+                Add -> eval xs $ execOpNumber (+) state
+                Min -> eval xs $ execOpNumber (-) state
+                Mul -> eval xs $ execOpNumber (*) state
+                Div -> eval xs $ execOpNumber (div) state
         BoolOp op ->
             case op of
-                Less -> eval xs $ execOpBool (<) stack
-                LessEq -> eval xs $ execOpBool (<=) stack
-                Greater -> eval xs $ execOpBool (>) stack
-                GreaterEq -> eval xs $ execOpBool (>=) stack
-                Equal -> eval xs $ execOpBool (==) stack
-                Different -> eval xs $ execOpBool (/=) stack
+                Less -> eval xs $ execOpBool (<) state
+                LessEq -> eval xs $ execOpBool (<=) state
+                Greater -> eval xs $ execOpBool (>) state
+                GreaterEq -> eval xs $ execOpBool (>=) state
+                Equal -> eval xs $ execOpBool (==) state
+                Different -> eval xs $ execOpBool (/=) state
         StackOp op ->
             case op of
-                Drop -> eval xs $ tail stack
-                Dup -> eval xs (head stack:stack)
-                Swap -> eval xs $ swap stack
+                Drop -> eval xs $ state { getStack = tail $ getStack state }
+                Dup -> eval xs $ state { getStack = (head (getStack state):(getStack state)) }
+                Swap -> eval xs $ state { getStack = swap (getStack state) }
+        AssignVariable name -> eval xs $ addVariable name state
+        GetVariable name -> eval xs $ appendVariableStack name state
 
 prog1 :: String
 prog1 = "1 2 +"
 
-exec :: Programme -> Stack
-exec = flip eval []
+exec :: Programme -> State
+exec = flip eval $ State [] Map.empty
+
+execStack :: Programme -> Stack
+execStack = getStack . exec
+
+execVariables :: Programme -> Variables
+execVariables = getVariable . exec
 
 test1, test2, test3, test4, test5, test6, test7 :: Bool
-test1 = exec [Nb (Decimal 1), Nb (Decimal 2), NumberOp Add] == [Nb (Decimal 3)]
-test2 = exec [Nb (Decimal 1), Nb (Decimal 2), NumberOp Min] == [Nb (Decimal 1)]
-test3 = exec [Nb (Decimal 3), Nb (Decimal 2), NumberOp Mul] == [Nb (Decimal 6)]
-test4 = exec [Nb (Decimal 2), Nb (Decimal 4), NumberOp Div] == [Nb (Decimal 2)]
-test5 = exec [Nb (Decimal 1), Nb (Decimal 2), BoolOp Less] == [Nb (Decimal 0)]
-test6 = exec [Nb (Decimal 2), Nb (Decimal 1), BoolOp Less] == [Nb (Decimal 1)]
-test7 = exec [Nb (Decimal 2), Nb (Decimal 2), BoolOp LessEq] == [Nb (Decimal 1)]
+test1 = execStack [Nb (Decimal 1), Nb (Decimal 2), NumberOp Add] == [Nb (Decimal 3)]
+test2 = execStack [Nb (Decimal 1), Nb (Decimal 2), NumberOp Min] == [Nb (Decimal 1)]
+test3 = execStack [Nb (Decimal 3), Nb (Decimal 2), NumberOp Mul] == [Nb (Decimal 6)]
+test4 = execStack [Nb (Decimal 2), Nb (Decimal 4), NumberOp Div] == [Nb (Decimal 2)]
+test5 = execStack [Nb (Decimal 1), Nb (Decimal 2), BoolOp Less] == [Nb (Decimal 0)]
+test6 = execStack [Nb (Decimal 2), Nb (Decimal 1), BoolOp Less] == [Nb (Decimal 1)]
+test7 = execStack [Nb (Decimal 2), Nb (Decimal 2), BoolOp LessEq] == [Nb (Decimal 1)]
 
 testStack1, testStack2, testStack3 :: Bool
-testStack1 = exec [Nb (Decimal 3), Nb (Decimal 4), StackOp Drop] == [Nb (Decimal 3)]
-testStack2 = exec [Nb (Decimal 3), Nb (Decimal 4), StackOp Dup] == [Nb (Decimal 4), Nb (Decimal 4), Nb (Decimal 3)]
-testStack3 = exec [Nb (Decimal 3), Nb (Decimal 4), StackOp Swap] == [Nb (Decimal 3), Nb (Decimal 4)]
+testStack1 = execStack [Nb (Decimal 3), Nb (Decimal 4), StackOp Drop] == [Nb (Decimal 3)]
+testStack2 = execStack [Nb (Decimal 3), Nb (Decimal 4), StackOp Dup] == [Nb (Decimal 4), Nb (Decimal 4), Nb (Decimal 3)]
+testStack3 = execStack [Nb (Decimal 3), Nb (Decimal 4), StackOp Swap] == [Nb (Decimal 3), Nb (Decimal 4)]
 
 allStackTests :: [Bool]
 allStackTests = [testStack1, testStack2, testStack3]

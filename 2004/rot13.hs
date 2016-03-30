00aea999b17b1f8aca8fe061c1797b3d973abbda
Sebastien Canart <kokotchy@gmail.com>
2016-03-30 19:37:31 +0200

    Finish rot13

--- 2004/rot13.hs
+++ 2004/rot13.hs
@@ -5,7 +5,14 @@ import Data.Char
 
 rot13 :: String -> String
 rot13 [] = []
-rot13 (x:xs) = []
+rot13 (x:xs)
+    | isAlpha x = c:rot13 xs
+    | otherwise = x:rot13 xs
+    where
+        base = if isUpper x
+            then ord 'A'
+            else ord 'a'
+        c = chr $ ((ord x - base + 13) `mod` 26) + base
 
 {-map (chr . (+ (ord 'a')) . (flip mod 26) . (+13) . ord) ['a'..'z']-}
 

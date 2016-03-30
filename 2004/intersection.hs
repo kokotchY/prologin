7aafc7f69b8aae02a58e0b0d2a135b1eb85105bd
Sebastien Canart <kokotchy@gmail.com>
2016-03-30 19:23:08 +0200

    Fix calculation of the difference, the periods might not be ordered

--- 2004/intersection.hs
+++ 2004/intersection.hs
@@ -2,25 +2,32 @@ module Main where
 
 import Control.Monad
 
-result :: Int -> Int -> Int -> Int -> Int
-result s_a e_a s_b e_b
-    | e_a >= s_b = abs $ e-s
+result :: (Integer, Integer) -> (Integer, Integer) -> Integer
+result a@(s_a,e_a) b@(s_b,e_b)
+    | snd first >= fst second = abs $ e-s
     | otherwise = 0
     where
-        s = max s_a s_b
-        e = min e_a e_b
+        (first, second) =
+            if s_a < s_b
+                then (a,b)
+                else (b,a)
+        s = max (fst first) (fst second)
+        e = min (snd first) (snd second)
 
-test1 :: Int
-test1 = result (-47686716) (-38491014) (-48528973) (-42411585)
+test1 :: Integer
+test1 = result ((-47686716),(-38491014)) ((-48528973),(-42411585))
 
-test2 :: Int
-test2 = result (-47698252) (-45630884) (-45440315) (-37180150)
+test2 :: Integer
+test2 = result ((-47698252),(-45630884)) ((-45440315),(-37180150))
+
+test3 :: Integer
+test3 = result ((-45440315),(-37180150)) ((-47698252),(-45630884))
 
 main :: IO ()
 main = do
     str_periode_a <- getLine
     str_periode_b <- getLine
-    let [s_a, e_a] = map read $ words str_periode_a :: [Int]
-        [s_b, e_b] = map read $ words str_periode_b :: [Int]
-    print $ result s_a e_a s_b e_b
+    let [s_a, e_a] = map read $ words str_periode_a :: [Integer]
+        [s_b, e_b] = map read $ words str_periode_b :: [Integer]
+    print $ result (s_a,e_a) (s_b,e_b)
     return ()

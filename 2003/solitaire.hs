f0251347a9b8fcb12d2fe992f9cf0cf79032bf7e
Sebastien Canart <kokotchy@gmail.com>
2016-03-30 16:51:32 +0200

    Add solitire solution

--- 2003/solitaire.hs
+++ 2003/solitaire.hs
@@ -1,11 +1,26 @@
 module Main where
 
 import Control.Monad
+import Data.List (transpose)
+
+count_pattern :: [Integer] -> Int
+count_pattern line = count_pattern' line + count_pattern' (reverse line)
+    where
+        count_pattern' :: [Integer] -> Int
+        count_pattern' (_:_:[]) = 0
+        count_pattern' p@(1:1:0:xs) = 1 + count_pattern' (tail p)
+        count_pattern' p = count_pattern' (tail p)
+
+get_grid :: IO [[Integer]]
+get_grid = replicateM 7 $ do
+    line <- getLine
+    return (map (\x -> read (x:"")) line :: [Integer])
+
+count_move :: [[Integer]] -> Int
+count_move grid = sum (map count_pattern grid) + sum (map count_pattern (transpose grid))
 
 main :: IO ()
 main = do
-    grid <- replicateM 7 $ do
-        line <- getLine
-        return (map (\x -> read (x:"")) line :: [Int])
-    print grid
+    grid <- get_grid
+    print $ count_move grid
     return ()

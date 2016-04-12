13d33f60b9ebde00585bdc2d73ce9f1364fd817d
Sebastien Canart <kokotchy@gmail.com>
2016-04-12 18:09:22 +0200

    Solve level freq_paires

--- 2010/freq_paires.hs
+++ 2010/freq_paires.hs
@@ -2,34 +2,35 @@ module Main where
 
 import Control.Monad
 import Data.Char
+import qualified Data.Map.Lazy as Map
+import Data.List
 
-generate_pair :: String -> [String]
-generate_pair [] = []
-generate_pair (x:[]) = []
-generate_pair (x:y:ys)
-    | is_valid x && is_valid y = [x:y:[]] ++ generate_pair (y:ys)
-    | otherwise = generate_pair (y:ys)
-    where
-        is_valid :: Char -> Bool
-        is_valid = not . flip elem [' ', '.']
+type Store = Map.Map String Integer
 
+incr_index :: Store -> String -> Store
+incr_index store key =
+    case Map.lookup key store of
+        Nothing -> Map.insert key 1 store
+        Just v -> Map.insert key (v+1) store
 
-generate_uniq_pair :: String -> [String]
-generate_uniq_pair = unique . generate_pair
+calc_occur :: [String] -> Store
+calc_occur = foldl' incr_index Map.empty
 
-occ :: Eq a => a -> [a] -> Int
-occ a = length . filter (==a)
+gen_list :: String -> [String]
+gen_list [x] = []
+gen_list (x:xs@(y:ys)) = [x:y:[]] ++ gen_list xs
 
-unique :: Eq a => [a] -> [a]
-unique = foldr (\x xs -> if not (x `elem` xs) then (x:xs) else xs) []
+get_most_occ :: String -> Integer
+get_most_occ = snd . maximumBy (\c1 c2 -> compare (snd c1) (snd c2)) . Map.toList . calc_occur . filter only_letters . gen_list
+
+only_letters :: String -> Bool
+only_letters = all (`elem` ['a'..'z'])
 
 main :: IO ()
 main = do
     _ <- getLine
     str_text <- getLine
     let text = map toLower str_text
-        pair = generate_pair text
-        uniq = unique pair
-        result = maximum $ map (flip occ (generate_pair text)) uniq
+        result = get_most_occ text
     print result
     return ()

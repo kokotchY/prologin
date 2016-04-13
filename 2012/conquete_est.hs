2b5326c00728815b046a0076710c967fad5b04d7
Sebastien Canart <kokotchy@gmail.com>
2016-04-13 22:02:47 +0200

    Add method to extract ascending list of numbers

--- 2012/conquete_est.hs
+++ 2012/conquete_est.hs
@@ -2,6 +2,15 @@ module Main where
 
 import Control.Monad
 
+group_ascending :: [Integer] -> [[Integer]]
+group_ascending (x:xs) = group_ascending' x [x] xs
+    where
+        group_ascending' :: Integer -> [Integer] -> [Integer] -> [[Integer]]
+        group_ascending' last acc [] = [acc]
+        group_ascending' last acc (x:xs)
+            | x >= last = group_ascending' x (x:acc) xs
+            | otherwise = acc : group_ascending' x [x] xs
+
 get_profit :: [Int] -> Int
 get_profit [] = 0
 get_profit (x:xs) = max (get_profit_for x xs) $ get_profit xs
